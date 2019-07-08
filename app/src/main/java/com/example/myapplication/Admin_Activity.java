062f42db687e1cf06c32c268ded2a5053792ee71
Hasan Tunahan <name_tunahan@hotmail.com>
2019-07-08 16:18:23 +0300

    düzenleme 08.07.2019 4:18

--- app/src/main/java/com/example/myapplication/Admin_Activity.java
+++ app/src/main/java/com/example/myapplication/Admin_Activity.java
@@ -1,14 +1,19 @@
 package com.example.myapplication;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
 import com.example.myapplication.Common.Common;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Admin_Activity extends AppCompatActivity {
 
-    private TextView kullanici_ismi;
+    private TextView sirketAdiAdminekran;
 
 
     @Override
@@ -17,14 +22,23 @@ public class Admin_Activity extends AppCompatActivity {
         setContentView(R.layout.activity_admin_);
 
     ///findbyViewid//
-    kullanici_ismi=findViewById(R.id.main2usernameEdittext);
-
+    sirketAdiAdminekran=findViewById(R.id.sirketiAdiAdminekran);
     ///////////////
 
-    ///appbarKullaniciİsmi gosterme
-        kullanici_ismi.setText(Common.currentUser.getName().toUpperCase());
-    ///////////////////////
+        //ŞirketAdi alıyoruz
+        FirebaseDatabase.getInstance().getReference("SirketBilgileri").child(Common.currentUser.getName()).child("SirketAdi").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String ad=dataSnapshot.getValue().toString();
+                sirketAdiAdminekran.setText("HOSGELDINIZ , "+ad);
+            }
 
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+////////////şirketadi
 
     }
 }
