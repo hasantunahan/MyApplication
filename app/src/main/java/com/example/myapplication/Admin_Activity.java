a457cfaf1a6e1470df6e9ad4244ae534c6065095
Hasan Tunahan <name_tunahan@hotmail.com>
2019-07-08 14:05:06 +0300

    düzenleme 08.07.2019 2:04

--- app/src/main/java/com/example/myapplication/Admin_Activity.java
+++ app/src/main/java/com/example/myapplication/Admin_Activity.java
@@ -2,12 +2,29 @@ package com.example.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.myapplication.Common.Common;
 
 public class Admin_Activity extends AppCompatActivity {
+
+    private TextView kullanici_ismi;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_);
+
+    ///findbyViewid//
+    kullanici_ismi=findViewById(R.id.main2usernameEdittext);
+
+    ///////////////
+
+    ///appbarKullaniciİsmi gosterme
+        kullanici_ismi.setText(Common.currentUser.getName().toUpperCase());
+    ///////////////////////
+
+
     }
 }
