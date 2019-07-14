d06229c3270ffae4c1311c64ef48982af149be00
hasantunahan <name_tunahan@hotmail.com>
2019-07-14 16:03:31 +0300

    düzenleme 14.07.2019 fav favlist finish

--- app/src/main/java/com/example/myapplication/ViewHolder/FavorilerAdapter.java
+++ app/src/main/java/com/example/myapplication/ViewHolder/FavorilerAdapter.java
@@ -8,9 +8,20 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import com.bumptech.glide.Glide;
+import com.example.myapplication.Common.Common;
+import com.example.myapplication.Database.Database;
+import com.example.myapplication.Favoriler;
+import com.example.myapplication.FoodList;
 import com.example.myapplication.Model.Food;
+import com.example.myapplication.Model.Order;
 import com.example.myapplication.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 import java.util.ArrayList;
@@ -20,6 +31,7 @@ public class FavorilerAdapter extends RecyclerView.Adapter<FavorilerAdapter.MyVi
 
     Context context;
     ArrayList<Food> list;
+    String foodid;
 
     public FavorilerAdapter(Context c,ArrayList<Food> a){
         context=c;
@@ -33,11 +45,48 @@ public class FavorilerAdapter extends RecyclerView.Adapter<FavorilerAdapter.MyVi
     }
 
     @Override
-    public void onBindViewHolder(@NonNull MyViewHolder myViewHolder, int i) {
+    public void onBindViewHolder(@NonNull final MyViewHolder myViewHolder, final int i) {
 
         myViewHolder.name.setText(list.get(i).getName().toUpperCase());
         myViewHolder.price.setText(list.get(i).getPrice());
         Glide.with(context).load(list.get(i).getImage()).into(myViewHolder.food_image);
+
+        FirebaseDatabase.getInstance().getReference("Favori").child(Common.currentUser.getName()).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                foodid= dataSnapshot.getKey();
+
+
+                myViewHolder.sepetEkleImage.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        new Database(context).addToChart(new Order(
+
+
+                                foodid,
+                                 list.get(i).getName(),
+                                "1",
+                                list.get(i).getPrice().toString(),
+                                list.get(i).getDiscount()
+
+
+
+                        ));
+
+                        Toast.makeText(context,"Sepete eklendi",Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
 
     }
